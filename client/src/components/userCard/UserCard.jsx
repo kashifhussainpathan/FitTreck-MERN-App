cff7bbf280aa6293bcfc7e52d58964cb1b3bb067
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-10-03 18:34:33 +0530

    feat: added logout button

--- client/src/components/userCard/UserCard.jsx
+++ client/src/components/userCard/UserCard.jsx
@@ -1,9 +1,19 @@
 import "./userCard.css";
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../actions/user.actions";
 
 function UserCard({ user }) {
+  const dispatch = useDispatch();
   const { username, profilePictureUrl, email, phoneNumber } = user;
+
+  const handleLogout = () => {
+    const emptyUser = {};
+    dispatch(setUser(emptyUser));
+    localStorage.removeItem("token");
+    localStorage.removeItem("isLoggedIn");
+  };
 
   return (
     <div className="usercard">
@@ -21,6 +31,7 @@ function UserCard({ user }) {
         <h3>@{username}</h3>
         <div>{email}</div>
         <div>{phoneNumber}</div>
+        <div onClick={handleLogout}>Logout </div>
         <div className="github">
           <a
             href="https://github.com/kashifhussainpathan/FitTreck-MERN-App"
