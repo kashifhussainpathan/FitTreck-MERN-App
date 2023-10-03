d14f75d376ddee3091f1937889f97582539a5ffe
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-10-03 16:14:45 +0530

    feat: added github link

--- client/src/components/userCard/UserCard.jsx
+++ client/src/components/userCard/UserCard.jsx
@@ -1,5 +1,6 @@
 import "./userCard.css";
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 function UserCard({ user }) {
   const { username, profilePictureUrl, email, phoneNumber } = user;
@@ -20,12 +21,12 @@ function UserCard({ user }) {
         <h3>@{username}</h3>
         <div>{email}</div>
         <div>{phoneNumber}</div>
-        <div>
+        <div className="github">
           <a
             href="https://github.com/kashifhussainpathan/FitTreck-MERN-App"
             target="_blank"
           >
-            Github
+            <AiFillGithub />
           </a>
         </div>
       </div>
