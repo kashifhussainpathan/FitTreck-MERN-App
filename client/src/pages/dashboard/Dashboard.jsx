f6fd8b091592181f7b024eb31adda54948aaaa70
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-10-05 00:26:32 +0530

    fix: fixed loading

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -9,6 +9,7 @@ import EChartPieComponent from "../../components/EChartPieComponent";
 import DashboardLoader from "../../components/loaders/DashboardLoader";
 
 function Dashboard() {
+  const user = useSelector((state) => state.userState.user);
   const foods = useSelector((state) => state.foodState.foods);
   const goals = useSelector((state) => state.goalState.goals);
   const exercises = useSelector((state) => state.exerciseState.exercises);
@@ -41,7 +42,12 @@ function Dashboard() {
     { value: remainingCaloriesToGoal, name: "Remaining Calories to Goal" },
   ];
 
-  if (isExercisesLoading && isFoodsLoading && isGoalsLoading) {
+  if (
+    Object.keys(user).length > 0 &&
+    isExercisesLoading &&
+    isFoodsLoading &&
+    isGoalsLoading
+  ) {
     return <DashboardLoader />;
   }
 
